b0004033c39ad9d400a79636a296d50ee8b649d3
TouseefCoder <umartouseef3333@gmail.com>
2025-09-29 12:02:30 +0500

    second change

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: '**',
-            },
-        ],
-    },
+  experimental: {
+    appDir: true, // ✅ Add this line
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
